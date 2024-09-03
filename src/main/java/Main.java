3917177ec45742ad8a27f4ccfefffd5167588e62
Gabrielle Dias <169068323+GABRIELLEDIAS19@users.noreply.github.com>
2024-09-03 02:25:11 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    int contador = 1; 
+    String nome = "Gabrielle";
+    
+    while (contador < 11) {
+      System.out.println("Bom dia!");
+    contador = contador + 1;
+    }
+
+    System.out.println(nome);
+  }
+  
+}
